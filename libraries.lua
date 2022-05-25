86b489935d7a81175853a7d643b5dbfc1701d13b
sulcovo <106101806+sulcovo@users.noreply.github.com>
2022-05-25 14:20:44 +0200

    Create libraries.lua

---
+++ libraries.lua
@@ -0,0 +1,3 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/sulcovo/sulcovorbx/main/mainlibrary.lua"))()
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/sulcovo/sulcovorbx/main/notholder.lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/sulcovo/sulcovorbx/main/notif.lua"))()
