a81822296d461cb5f221b11f5b802652deb68ea4
sulcovo <106101806+sulcovo@users.noreply.github.com>
2022-05-25 15:40:20 +0200

    Update infinitecurrencies.lua

--- infinitecurrencies.lua
+++ infinitecurrencies.lua
@@ -1,3 +1,3 @@
 for _,val in pairs(game:GetService'Players'.LocalPlayer.Status.Currencies:GetChildren()) do
-game:GetService'ReplicatedStorage'.RemoteEvents.MainFunction:InvokeServer("ChangeValue",{["NewValue"] = 0,["Obj"] = val})
+game:GetService'ReplicatedStorage'.RemoteEvents.MainFunction:InvokeServer("ChangeValue",{["NewValue"] = 696969696969,["Obj"] = val})
 end
